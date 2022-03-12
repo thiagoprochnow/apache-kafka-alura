8c64ae982acf77ea659a4c1bb5bb27c8a8e98b82
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2022-03-12 20:07:38 -0300

    Creating producers

---
+++ src/main/java/br/com/alura/EcommerceApplication.java
@@ -0,0 +1,39 @@
+package br.com.alura;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		SpringApplication.run(EcommerceApplication.class, args);
+		var producer = new KafkaProducer<String, String>(properties());
+		var value = "132123,67523,632643687326";
+		var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+		producer.send(record, (data, ex) -> {
+			if(ex != null) {
+				ex.printStackTrace();
+				return;
+			}
+			System.out.println("sucesso " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp" + data.timestamp());
+		}).get();
+	}
+
+	private static Properties properties() {
+		var properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return properties;
+	}
+
+}
